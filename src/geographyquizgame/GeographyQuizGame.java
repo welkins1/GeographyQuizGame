d76f23e0924eed2f9650a3a2848561f906e9c640
Austin <austin.welkins1@gmail.com>
2024-12-03 13:45:20 -0500

    Update GeographyQuizGame.java
    Combined the QuestionAnswerPair file with main program file. This makes it easier to run the program upon download.

--- src/geographyquizgame/GeographyQuizGame.java
+++ src/geographyquizgame/GeographyQuizGame.java
@@ -1,5 +1,3 @@
-package geographyquizgame;
-
 //Programmer Name: Austin
 //GitHub Username: welkins1
 
@@ -12,6 +10,33 @@ import java.util.ArrayList;
 
 //Used for randomization of questions
 import java.util.Collections;
+
+
+class QuestionAnswerPair {
+    
+    //Question variable holds the question, answer variable holds the answer 
+    private String question;
+    private String answer;
+    
+    
+    //This creates a question and answer pair 
+    public QuestionAnswerPair(String question, String answer) {
+        
+        //Using "this" makes it so the program specifically knows which variable/object to refer to,
+        this.question = question;
+        this.answer = answer;
+    }
+    
+    //Method to get the question
+    public String getQuestion () {
+        return question;
+    }
+    
+    //Method to get the answer 
+    public String getAnswer () {
+        return answer;
+    }
+}
 
 public class GeographyQuizGame {
 
