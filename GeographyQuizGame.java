e46cb09a04d37b8cc44f60726f4da83bc847ed87
Austin <austin.welkins1@gmail.com>
2024-12-18 21:20:25 -0500

    Update GeographyQuizGame.java
    Added more questions and fixed the ASCII globe formatting.

--- GeographyQuizGame.java
+++ GeographyQuizGame.java
@@ -49,25 +49,28 @@ public class GeographyQuizGame {
         //This while loop is used later to let the user continue playing / restart the program
         while (PlayAgain) {
            
-            System.out.println("            _.-,=_\"\"\"--,_\n" +
-"        .-\" =/7\"   _  .3#\"=.\n" +
-"      ,#7  \" \"  ,//)#d#######=.\n" +
-"    ,/ \"      # ,i-/###########=\n" +
-"   /         _)#sm###=#=# #######\\\n" +
-"  /         (#/\"_`;\\//#=#\\-#######\\\n" +
-" /         ,d####-_.._.)##P########\\\n" +
-",        ,\"############\\\\##bi- `\\| Y.\n" +
-"|       .d##############b\\##P'   V  |\n" +
-"|\\      '#################!\",       |\n" +
-"|C.       \\###=############7        |\n" +
-"'###.           )#########/         '\n" +
-" \\#(             \\#######|         /\n" +
-"  \\B             /#######7 /      /\n" +
-"   \\             \\######\" /\"     /\n" +
-"    `.            \\###7'       ,'\n" +
-"      \"-_          `\"'      ,-'\n" +
-"         \"-._           _.-\"\n" +
-"             \"\"\"\"---\"\"\"\"");
+         System.out.println("""
+ 
+        _.-,=_"""--,_
+        .-" =/7"   _  .3#"=.
+      ,#7  " "  ,//)#d#######=.
+    ,/ "      # ,i-/###########=
+   /         _)#sm###=#=# #######\
+  /         (#/"_`;\//#=#\-#######\
+ /         ,d####-_.._.)##P########\
+,        ,"############\\##bi- `\| Y.
+|       .d##############b\##P'   V  |
+|\      '#################!",       |
+|C.       \###=############7        |
+'###.           )#########/         '
+ \#(             \#######|         /
+  \B             /#######7 /      /
+   \             \######" /"     /
+    `.            \###7'       ,'
+      "-_          `"'      ,-'
+         "-._           _.-"
+             """"---""""
+                        """);
             
             //Credit to Martin Schaffner for this Globe via ascii.co.uk
             
@@ -92,11 +95,63 @@ public class GeographyQuizGame {
 
             EasyQA.add(new QuestionAnswerPair("What continent is Canada in? ", "North America"));
 
+            EasyQA.add(new QuestionAnswerPair("What is the largest ocean on Earth? ", "Pacific Ocean"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the capital of Italy? ", "Rome"));
+
+            EasyQA.add(new QuestionAnswerPair("Which country is famous for the Eiffel Tower? ", "France"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the smallest country in the world? ", "Vatican City"));
+
+            EasyQA.add(new QuestionAnswerPair("What continent is Australia in? ", "Australia"));
+
+            EasyQA.add(new QuestionAnswerPair("Which country is known as the Land of the Rising Sun? ", "Japan"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the capital of Canada? ", "Ottawa"));
+
+            EasyQA.add(new QuestionAnswerPair("Which desert is the largest in the world? ", "Sahara Desert"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the capital of Mexico? ", "Mexico City"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the name of the longest river in the world? ", "Nile River"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the capital of Japan? ", "Tokyo"));
+
+            EasyQA.add(new QuestionAnswerPair("Which country is famous for its pyramids? ", "Egypt"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the largest country in the world by land area? ", "Russia"));
+
+            EasyQA.add(new QuestionAnswerPair("What continent is Brazil in? ", "South America"));
+
+            EasyQA.add(new QuestionAnswerPair("Which country is known for its maple syrup? ", "Canada"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the capital of Spain? ", "Madrid"));
+
+            EasyQA.add(new QuestionAnswerPair("Which U.S. state is known as the Sunshine State? ", "Florida"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the capital of India? ", "New Delhi"));
+
+            EasyQA.add(new QuestionAnswerPair("Which continent is home to the Amazon Rainforest? ", "South America"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the primary language spoken in Brazil? ", "Portuguese"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the capital of the United Kingdom? ", "London"));
+    
+            EasyQA.add(new QuestionAnswerPair("Which ocean lies to the west of the United States? ", "Pacific Ocean"));
+
+            EasyQA.add(new QuestionAnswerPair("Which country is famous for its Great Wall? ", "China"));
+
+            EasyQA.add(new QuestionAnswerPair("What is the smallest continent by land area? ", "Australia"));
+
+
+
             //Create array list for MediumQA
             ArrayList<QuestionAnswerPair> MediumQA = new ArrayList<>();
 
             //Adds questions to MediumQA 
             MediumQA.add(new QuestionAnswerPair("What continent is Egypt in? ", "Africa"));
+
+            MediumQA.add(new QuestionAnswerPair("What is the smallest country in the world? ", "Vatican City"));
 
             MediumQA.add(new QuestionAnswerPair("What country has the 3rd largest population? ", "United States of America"));
 
@@ -104,17 +159,79 @@ public class GeographyQuizGame {
 
             MediumQA.add(new QuestionAnswerPair("Which of the following countries do not have red, white, or blue on their flag?\n \nRussia, Japan, Indonesia, or Jamaica ", "Jamaica"));
 
+            MediumQA.add(new QuestionAnswerPair("What is the longest river in South America? ", "Amazon River"));
+
+            MediumQA.add(new QuestionAnswerPair("What is the capital of South Korea? ", "Seoul"));
+
+            MediumQA.add(new QuestionAnswerPair("Which country is the largest producer of coffee? ", "Brazil"));
+
+            MediumQA.add(new QuestionAnswerPair("What mountain range is Mount Everest a part of? ", "Himalayas"));
+
+            MediumQA.add(new QuestionAnswerPair("What is the capital of Kenya? ", "Nairobi"));
+
+            MediumQA.add(new QuestionAnswerPair("What country is home to the ancient city of Petra? ", "Jordan"));
+
+            MediumQA.add(new QuestionAnswerPair("What is the official language of Egypt? ", "Arabic"));
+
+            MediumQA.add(new QuestionAnswerPair("Which country has the most UNESCO World Heritage Sites? ", "Italy"));
+
+            MediumQA.add(new QuestionAnswerPair("What is the capital of Argentina? ", "Buenos Aires"));
+
+            MediumQA.add(new QuestionAnswerPair("Which African country has the highest population? ", "Nigeria"));
+
+            MediumQA.add(new QuestionAnswerPair("What is the capital of Saudi Arabia? ", "Riyadh"));
+
+            MediumQA.add(new QuestionAnswerPair("Which U.S. state is closest to the continent of Asia? ", "Alaska"));
+
+            MediumQA.add(new QuestionAnswerPair("What is the largest island in the world? ", "Greenland"));
+
+
             //Creates array list for HardQA
             ArrayList<QuestionAnswerPair> HardQA = new ArrayList<>();
 
             //Adds Questiosn to HardQA
             HardQA.add(new QuestionAnswerPair("What is the capital of Burkina Faso? ", "Ouagadougou"));
 
-            HardQA.add(new QuestionAnswerPair("What is the smallest country in the world? ", "Vatican City"));
-
             HardQA.add(new QuestionAnswerPair("What continent is Palau located in? ", "Oceania"));
 
             HardQA.add(new QuestionAnswerPair("What country has the most natural lakes? ", "Canada"));
+
+            HardQA.add(new QuestionAnswerPair("What is the name of the mountain range that separates Europe and Asia? ", "Ural Mountains"));
+
+            HardQA.add(new QuestionAnswerPair("What is the primary currency used in South Africa? ", "South African Rand"));
+
+            HardQA.add(new QuestionAnswerPair("What is the second largest desert in the world? ", "Arctic Desert"));
+
+            HardQA.add(new QuestionAnswerPair("What is the capital of Bhutan? ", "Thimphu"));
+
+            HardQA.add(new QuestionAnswerPair("Which country is home to the K2 mountain? ", "Pakistan"));
+
+            HardQA.add(new QuestionAnswerPair("What is the smallest country in South America by land area? ", "Suriname"));
+
+            HardQA.add(new QuestionAnswerPair("What is the capital of Kyrgyzstan? ", "Bishkek"));
+
+            HardQA.add(new QuestionAnswerPair("What is the name of the deepest part of the world's oceans? ", "Mariana Trench"));
+
+            HardQA.add(new QuestionAnswerPair("What is the capital of Tajikistan? ", "Dushanbe"));
+
+            HardQA.add(new QuestionAnswerPair("Which African country has the longest coastline? ", "Somalia"));
+
+            HardQA.add(new QuestionAnswerPair("What is the name of the longest mountain range in the world? ", "Andes Mountains"));
+
+            HardQA.add(new QuestionAnswerPair("What is the capital of Papua New Guinea? ", "Port Moresby"));
+
+            HardQA.add(new QuestionAnswerPair("What is the name of the river that flows through Baghdad? ", "Tigris River"));
+
+            HardQA.add(new QuestionAnswerPair("Which country is known as the Land of a Thousand Hills? ", "Rwanda"));
+
+            HardQA.add(new QuestionAnswerPair("What is the capital of Eritrea? ", "Asmara"));
+
+            HardQA.add(new QuestionAnswerPair("What is the largest lake in Africa? ", "Lake Victoria"));
+
+            HardQA.add(new QuestionAnswerPair("What is the capital of Moldova? ", "Chisinau"));
+
+            HardQA.add(new QuestionAnswerPair("What is the only country that shares a land border with Brunei? ", "Malaysia"));
+
 
             //Array place holder for Difficulty + corresponding answers, a value of null is used to ensure the Array has no questions, but questions can be added
             ArrayList<QuestionAnswerPair> selectedQA = null;
